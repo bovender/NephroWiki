a9c4c107ea2a027c84e414584b7c5ef84ced1ceb
Daniel Kraus <krada@gmx.net>
2013-08-02 11:03:22 +0200

    Initial commit of NephroWiki.body.php.

---
+++ NephroWiki.body.php
@@ -0,0 +1,17 @@
+<?php
+# NephroWiki extension for MediaWiki
+# (c) 2013 Daniel Kraus
+#
+# This file contains the NephroWiki class which defines the loadModule
+# method that is hooked on to OutputPageParserOutput. It serves to add
+# the resource loader module which loads NephroWiki's own JavaScript.
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+class NephroWiki {
+	function loadModule($out, $parseroutput) {
+		$out->addModules('ext.nephrowiki');
+	}
+}
